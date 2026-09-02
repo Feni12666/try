42d31fb46c5e2c511a285c914fcb3cc1c164cd18
Feni12666 <Feni12666@users.noreply.github.com>
2026-09-02 06:07:31 +0400

    Fix Direct APK workflow and Shizuku Parcelable

--- app/src/main/java/com/nagram/usbbridge/shizuku/RemoteFileEntry.kt
+++ app/src/main/java/com/nagram/usbbridge/shizuku/RemoteFileEntry.kt
@@ -1,13 +1,35 @@
 package com.nagram.usbbridge.shizuku
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class RemoteFileEntry(
+class RemoteFileEntry(
     val absolutePath: String,
     val displayName: String,
     val directory: Boolean,
     val sizeBytes: Long,
     val lastModified: Long,
-) : Parcelable
+) : Parcelable {
+    private constructor(parcel: Parcel) : this(
+        absolutePath = parcel.readString().orEmpty(),
+        displayName = parcel.readString().orEmpty(),
+        directory = parcel.readInt() != 0,
+        sizeBytes = parcel.readLong(),
+        lastModified = parcel.readLong(),
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(absolutePath)
+        parcel.writeString(displayName)
+        parcel.writeInt(if (directory) 1 else 0)
+        parcel.writeLong(sizeBytes)
+        parcel.writeLong(lastModified)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<RemoteFileEntry> {
+        override fun createFromParcel(parcel: Parcel) = RemoteFileEntry(parcel)
+        override fun newArray(size: Int): Array<RemoteFileEntry?> = arrayOfNulls(size)
+    }
+}
